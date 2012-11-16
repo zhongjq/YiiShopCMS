c4dbf96c333fe75a666a17f1b79bbf9896ed3e6b
enchikiben <enchikiben@enchikiben>
2012-11-16 23:19:03 +0600

    учзщке

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // theme
 define('YII_THEME','m-shina59');
 // connectionString
-define('CONNECTION_STRING','mysql:host=mysql0.db.koding.com;dbname=enchikiben_fbfde');
+define('CONNECTION_STRING','mysql:host=localhost;dbname=enchikiben_fbfde');
 // username
 define('DB_USERNAME','enchikiben_fbfde');
 // password
